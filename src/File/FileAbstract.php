be09554586588711a903c329cad0145a3a69f72b
Joseph Zhao <joseph.zhao@xing.net.au>
2017-01-19 14:48:39 +1100

    Add file validator class.

--- src/File/FileAbstract.php
+++ src/File/FileAbstract.php
@@ -18,6 +18,8 @@ abstract class FileAbstract implements FileInterface
 {
     private $_file;
     private $_archive;
+    private $_ignored = [];
+    private $_invalid_extensions = [];
 
     /**
      * FileAbstract constructor.
@@ -53,7 +55,31 @@ abstract class FileAbstract implements FileInterface
 
         $this->setArchive($archive);
 
+        // Set ignored.
+        $this->setIgnored();
+
         return $archive;
+    }
+
+    /**
+     * @param $file
+     */
+    public function validate($file)
+    {
+        if (empty($file)) {
+            throw new InvalidArgumentException(FileStatus::getStatus(ZipArchive::ER_NOENT));
+        }
+
+        try {
+            $archive = new ZipArchive();
+            $open = $archive->open($file, ZipArchive::CHECKCONS);
+
+
+        } catch (RuntimeException $exception) {
+            throw $exception;
+        }
+
+        return true;
     }
 
     /**
@@ -99,11 +125,42 @@ abstract class FileAbstract implements FileInterface
             if ($name === false) {
                 throw new RuntimeException(FileStatus::getStatus($this->_archive->status));
             }
+
+            // Handle ignore item.
+            if (!empty($this->_ignored) && $this->checkIgnore($name) === true) {
+                // Remove this file from archive.
+                $this->remove($name);
+                continue;
+            }
+
             // Add file into lists.
             array_push($list, $name);
         }
 
         return $list;
+    }
+
+    /**
+     * Return lists as tree.
+     * @return array
+     */
+    public function listsTree()
+    {
+        $list = $this->lists();
+        $tree = [];
+
+        foreach ($list as $file_path) {
+            $parts = explode('/', $file_path);
+            // Files in archive end in / if a directory.
+            if (substr($file_path, -1) === '/') {
+                $parts = array_slice($parts, 0, -1);
+                \Minisite\minisite_array_set_nested_value($tree, $parts, array('.' => $file_path));
+            } else {
+                \Minisite\minisite_array_set_nested_value($tree, $parts, $file_path);
+            }
+        }
+
+        return $tree;
     }
 
     /**
@@ -149,5 +206,64 @@ abstract class FileAbstract implements FileInterface
     public function setArchive(ZipArchive $archive)
     {
         $this->_archive = $archive;
+    }
+
+    /**
+     * @return array
+     */
+    public function getIgnored()
+    {
+        foreach ($this->_ignored as $key => $item) {
+            $this->_ignored[$key] = preg_quote($item);
+        }
+
+        return $this->_ignored;
+    }
+
+    /**
+     * @param array $ignored
+     */
+    public function setIgnored(array $ignored = [])
+    {
+        $ignored_default = [
+            '.svn',
+            '__macosx',
+            '.DS_Store',
+            '.dropbox',
+            'thumbs.db',
+            'desktop.ini',
+        ];
+        $this->_ignored = $ignored_default + $ignored;
+    }
+
+    /**
+     * @param $file
+     * @return bool
+     */
+    protected function checkIgnore($file)
+    {
+        // Handle ignored item.
+        $pattern = '/('.implode('|', $this->_ignored).')/i';
+        if (preg_match($pattern, $file)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * @return array
+     */
+    public function getInvalidExtensions()
+    {
+        return $this->_invalid_extensions;
+    }
+
+    /**
+     * @param array $invalid_extensions
+     */
+    public function setInvalidExtensions(array $invalid_extensions = [])
+    {
+        $this->_invalid_extensions = $invalid_extensions;
     }
 }
